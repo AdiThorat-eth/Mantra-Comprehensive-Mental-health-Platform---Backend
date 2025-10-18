6562e2aaebeab1c320f616cd6085ea3bfd473851
SanketLohar <loharsanket105@gmail.com>
2025-10-18 22:20:13 +0530

    updated code

--- project/src/main/java/com/mantra/MantraApplication.java
+++ project/src/main/java/com/mantra/MantraApplication.java
@@ -2,9 +2,13 @@ package com.mantra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.mantra")
+@EnableJpaRepositories(basePackages = "com.mantra")
+@EntityScan(basePackages = "com.mantra")
 @EnableJpaAuditing
 public class MantraApplication {
     public static void main(String[] args) {
